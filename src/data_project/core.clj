983767c4791d1162097ed8f97c37a30f463ba276
Lonnie Souder <lonnie@souderfamily.net>
2017-03-16 14:56:48 +0000

    mytest is able to parse the data into a list of hash-maps using load and makerecord.

--- src/data_project/core.clj
+++ src/data_project/core.clj
@@ -25,3 +25,21 @@
   (with-open [rdr (io/reader file)]
          (reduce merge {} (doall(map processLine (line-seq rdr)))))
   )
+
+(defn load 
+  [file]
+  (str/split (slurp file) #"\n"))
+
+(defn makerecord 
+  [thedata]
+  (if (> (count thedata) 1)
+  (reduce merge {} (map #(let  [line %1 splitline (str/split line #":")]
+        { (keyword (nth splitline 0)) (nth splitline 1)})
+      thedata))
+  )
+)
+
+(defn mytest 
+  [file]
+  (remove nil? (map makerecord (partition-by empty? (load file)) ))
+)
