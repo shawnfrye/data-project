a6b30ca539817abd2662141a33990deec6853478
Lonnie Souder <lonnie@souderfamily.net>
2017-04-20 14:42:35 -0400

    output test html.  add the box function which makes an html box of any color

--- src/data_project/core.clj
+++ src/data_project/core.clj
@@ -1,6 +1,7 @@
 (ns data-project.core
 (:require [clojure.string :as str])
 (:require [clojure.java.io :as io])
+(:require [hiccup.core :as h])
       (:gen-class))
 
 (defn load-txt 
@@ -24,10 +25,25 @@
         (let [g-name (first g) values (second g) ]
              (println g-name " " (count values)))))
 
+(def box-style 
+  "width:20px;height:20px;
+  margin:5px;
+  border:1px solid rgba(0, 0, 0, .2);background-color:")
+
+(defn box [color] (assoc {:class "box"} :style (str box-style color)))
+
+(defn test-html [name] (h/html [:html 
+ [:div {:class "input-color"}[:input {:type "text" :value "Orange"}]
+                             [:div (box "green") ]
+                             [:div (box "red") ]
+                             [:div (box "blue") ]
+                             [:div (box "purple") ]
+                             ]]))
 
 (defn -main
   "this program will read, sort and display the test data"
   [& args]
   (do 
   (println "reading data...")
-  (read-data (first args))))
+  (spit "test.html" (test-html "lonnie"))))
+  ;;(read-data (first args))))
