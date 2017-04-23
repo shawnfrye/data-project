facdd3eaecc5048dee61f9b021630b8cac9312e8
Lonnie Souder <lonnie@souderfamily.net>
2017-04-23 17:04:37 -0400

    fix comments.  pass all of the data to create-html.

--- src/data_project/core.clj
+++ src/data_project/core.clj
@@ -6,28 +6,29 @@
       (:gen-class))
 
 (defn load-txt 
-"load the text and split the data by lr's"
+"loads the text and returns list of strings. Each string is a line."
   [file]
   (str/split (slurp file) #"\n"))
 
 (defn makekvpair
-"create a key value pair from a : delimited string where the 1st value is the key and the second is the value"
-[line]
-(let [splitline(str/split line #":")]
-  {(keyword (nth splitline 0)) (nth splitline 1)}))
+"makes a key value pair from a : delimited string where the 1st value is the key and the second is the value"
+  [line]
+  (let [splitline(str/split line #":")]
+    {(keyword (nth splitline 0)) (nth splitline 1)}))
 
 (defn makerecord 
+"makes a record from a list of strings.  Each string is a : delimited kv pair."
   [thedata]
   (if (> (count thedata) 1)
   (reduce merge {} (map makekvpair thedata))))
 
 (defn read-data 
-"read the file and returns a list of records"
+"reads the file and return a list of records."
   [file]
   (remove nil? (map makerecord (partition-by empty? (load-txt file)) )))
 
 (defn count-by-color
-"test function which will print out the number of records in each color"
+"test function which will print out the number of records in each color."
   [file]
   (for [g (group-by :Color (read-data file))]
         (let [g-name (first g) values (second g) ]
@@ -42,13 +43,14 @@
   [:div (assoc {:class "box"} :style (str box-style color) :onclick (str "alert('" color "')"))])
 
 (defn create-html 
-"return a static html page which can display the data which is grouped by color"
-  [colors] 
-  (hp/html5 (into [:div] (map box colors )))) 
+"returns a static html page which can display the data grouped by color."
+  [data] 
+  (let [colors (keys (group-by :Color data))]
+  (hp/html5 (into [:div] (map box colors )))))
 
 (defn -main
   "this program will read, sort and display the test data"
   [& args]
   (do
   (println "reading data...")
-  (spit "test.html" (create-html (keys (group-by :Color (read-data (first args))))))))
+  (spit "test.html" (create-html (read-data (first args))))))
