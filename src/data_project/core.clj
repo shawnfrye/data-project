3585e736cab8b1deed3590d1f8b65cb21f02175f
Lonnie Souder <lonnie@souderfamily.net>
2017-04-22 22:53:26 -0400

    refactor makerecord by adding makekvpair. add comments and add some tests

--- src/data_project/core.clj
+++ src/data_project/core.clj
@@ -6,40 +6,49 @@
       (:gen-class))
 
 (defn load-txt 
+"load the text and split the data by lr's"
   [file]
   (str/split (slurp file) #"\n"))
+
+(defn makekvpair
+"create a key value pair from a : delimited string where the 1st value is the key and the second is the value"
+[line]
+(let [splitline(str/split line #":")]
+  {(keyword (nth splitline 0)) (nth splitline 1)}))
 
 (defn makerecord 
   [thedata]
   (if (> (count thedata) 1)
-  (reduce merge {} (map #(let  [line %1 splitline (str/split line #":")]
-        { (keyword (nth splitline 0)) (nth splitline 1)})
-      thedata))))
+  (reduce merge {} (map makekvpair thedata))))
 
 (defn read-data 
+"read the file and returns a list of records"
   [file]
   (remove nil? (map makerecord (partition-by empty? (load-txt file)) )))
 
 (defn count-by-color
+"test function which will print out the number of records in each color"
   [file]
   (for [g (group-by :Color (read-data file))]
         (let [g-name (first g) values (second g) ]
              (println g-name " " (count values)))))
 
 (def box-style 
-  "width:20px;height:20px;
-  margin:5px;
-  border:1px solid rgba(0, 0, 0, .2);background-color:")
+  "width:20px;height:20px; margin:5px; border:1px solid rgba(0, 0, 0, .2);background-color:")
 
-(defn box [color] [:div (assoc {:class "box"} :style (str box-style color) :onclick (str "alert('" color "')"))])
+(defn box 
+"returns a 20x20px colored div and alert. color arg is used to color the box and specify the group to show."
+  [color] 
+  [:div (assoc {:class "box"} :style (str box-style color) :onclick (str "alert('" color "')"))])
 
-(defn test-html [color] (hp/html5 
-    (into [:div] (map box color )))) 
+(defn create-html 
+"return a static html page which can display the data which is grouped by color"
+  [colors] 
+  (hp/html5 (into [:div] (map box colors )))) 
 
 (defn -main
   "this program will read, sort and display the test data"
   [& args]
   (do
   (println "reading data...")
-  ;;(keys (group-by :Color (read-data (first args)))))
-  (spit "test.html" (test-html (keys (group-by :Color (read-data (first args))))))))
+  (spit "test.html" (create-html (keys (group-by :Color (read-data (first args))))))))
--- test/data_project/core_test.clj
+++ test/data_project/core_test.clj
@@ -2,6 +2,16 @@
   (:require [clojure.test :refer :all]
             [data-project.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest makekvpair-test
+  (testing "single delimited string should return a key/value pair"
+    (is (= (makekvpair "Color:orange") {:Color "orange"}))))
+
+(deftest makerecord-test
+  (testing "take a list of : delimited strings return a  map"
+    (is (= (makerecord ["Name:test mctest" "Color:orange"]) {:Name "test mctest" :Color "orange"}))))
+
+(def blue-box [:div {:class "box", :style "width:20px;height:20px; margin:5px; border:1px solid rgba(0, 0, 0, .2);background-color:blue", :onclick "alert('blue')"}])
+
+(deftest box-test
+  (testing "box html test"
+    (is (= (box "blue") blue-box))))
